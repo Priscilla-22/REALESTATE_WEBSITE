460246019e64a8ffd1f7d18cbedac2c6ab33bace
Priscilla-22 <priscillamw123@gmail.com>
2023-10-22 15:58:19 +0300

    Created a login/signup form

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function toggleDarkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+ const formOpenBtn = document.querySelector("#form-open"),
+ loginForm = document.querySelector(".loginForm"),
+ formContainer = document.querySelector(".form-container"),
+ formCloseBtn = document.querySelector(".form-close"),
+ signupBtn = document.querySelector("#signup"),
+ loginBtn = document.querySelector("#login"),
+ pwShowHide = document.querySelectorAll(".pw-hide"); 
+
+ formOpenBtn.addEventListener("click", () => loginForm.classList.add("show") )
+ formCloseBtn.addEventListener("click", () => loginForm.classList.remove("show") )
