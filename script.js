d509c57185136c23a22f13b774286ef725e5f2ba
Priscilla-22 <priscillamw123@gmail.com>
2023-10-24 13:11:31 +0300

    wrapped the js code for the subscribe form in a 'DOMContentLoaded'

--- script.js
+++ script.js
@@ -4,45 +4,45 @@ function toggleDarkMode() {
   element.classList.toggle("dark-mode");
 }
 
-// Element selection
-const formOpenBtn = document.querySelector("#form-open");
-  loginForm = document.querySelector(".loginForm");
-  formContainer = document.querySelector(".signup-form");
-  formCloseBtn = document.querySelector(".form-close");
-  signupBtn = document.querySelector("#signup");
-  loginBtn = document.querySelector("#login");
-  pwShowHide = document.querySelectorAll(".pw-hide");
-
-// Event listeners
-formOpenBtn.addEventListener("click", () => {
-  loginForm.classList.add("show");
-});
-formCloseBtn.addEventListener("click", () => {
-  loginForm.classList.remove("show");
-});
-
-pwShowHide.forEach(icon => {
-  icon.addEventListener("click", () => {
-    let getPwInput = icon.parentElement.querySelector("input");
-  if(getPwInput.type === "password") {
-    getPwInput.type = "text";
-    icon.classList.replace("fa-eye-slash", "fa-eye");
-  }else{
-    getPwInput.type = "password";
-    icon.classList.replace("fa-eye", "fa-eye-slash");
-  }
-  })
+document.addEventListener("DOMContentLoaded", function () {
+  // Element selection
+  const formOpenBtn = document.querySelector("#form-open");
+  const subscribeForm = document.querySelector(".subscribe-form");
+  const formContainer = document.querySelector(".form-container");
+  const formCloseBtn = document.querySelector(".form-close");
+  const signupBtn = document.querySelector("#signup");
+  const loginBtn = document.querySelector("#login");
+  const pwShowHide = document.querySelectorAll(".pw-hide");
   
+  // Event listeners
+   formOpenBtn.addEventListener("click", () => {
+    subscribeForm.classList.add("show");
+  });
+  formCloseBtn.addEventListener("click", () => {
+    subscribeForm.classList.remove("show");
+  });
+  
+  pwShowHide.forEach(icon => {
+    icon.addEventListener("click", () => {
+      let getPwInput = icon.parentElement.querySelector("input");
+      if (getPwInput.type === "password") {
+        getPwInput.type = "text";
+        icon.classList.replace("fa-eye-slash", "fa-eye");
+      } else {
+        getPwInput.type = "password";
+        icon.classList.replace("fa-eye", "fa-eye-slash");
+      }
+    });
+  });
+  
+  signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+  });
+  
+  loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+  });
 });
 
-
-
-signupBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.add("active");
-});
-
-loginBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  formContainer.classList.remove("active");
-});
